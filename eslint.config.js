4619e48438293f958ea50060f108b8c72ccaf6e6
b.karamov <karamov.bi@phystech.edu>
2025-06-01 15:45:00 +0300

    Починил lint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        chrome: 'readonly',
+        DOMPurify: 'readonly',
+        marked: 'readonly'
+      },
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-unused-vars': ['warn'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }]
+    }
+  }
+]; 
